e2480a429197a30d54f9a9e140b98a9baa48d995
Said van de Klundert <said.van.de.klundert@ibm.com>
2022-01-03 21:50:30 +0100

    adding s3 stuffz

--- s3/s3_create_and_delete_bucket.py
+++ s3/s3_create_and_delete_bucket.py
@@ -1,23 +1,26 @@
+import os
 import uuid
 import boto3
 
 uuid_string = str(uuid.uuid4())
+BUCKET_NAME = f"example-{uuid_string}"
 s3_resource = boto3.resource("s3")
 s3_client = boto3.client("s3")
+
 # Create a bucket:
 resp_bucket_creation = s3_resource.create_bucket(
-    Bucket=f"example-{uuid_string}",
+    Bucket=BUCKET_NAME,
     CreateBucketConfiguration={"LocationConstraint": "eu-central-1"},
 )
 
 # Turn on versioning for the bucket:
-bucket_versioning = s3_resource.BucketVersioning(f"example-{uuid_string}")
+bucket_versioning = s3_resource.BucketVersioning(BUCKET_NAME)
 versioning_response = bucket_versioning.enable()
-versioning_response = s3_client.get_bucket_versioning(Bucket=f"example-{uuid_string}")
+versioning_response = s3_client.get_bucket_versioning(Bucket=BUCKET_NAME)
 
 # Turn on encryption, cannot be done using resource, so we turn to the client:
 response = s3_client.put_bucket_encryption(
-    Bucket=f"example-{uuid_string}",
+    Bucket=BUCKET_NAME,
     ServerSideEncryptionConfiguration={
         "Rules": [
             {
@@ -28,9 +31,36 @@ response = s3_client.put_bucket_encryption(
         ]
     },
 )
-encryption_response = s3_client.get_bucket_encryption(Bucket=f"example-{uuid_string}")
+encryption_response = s3_client.get_bucket_encryption(Bucket=BUCKET_NAME)
 
-# delete the bucket:
-# import time
-# time.sleep(120)
-delete_response = s3_resource.Bucket(f"example-{uuid_string}").delete()
+#
+s3_client = boto3.client("s3")
+
+# upload a file:
+print("Uploading the file.")
+dirname = os.path.dirname(__file__)
+file_to_upload = os.path.join(dirname, "files/example.txt")
+with open(file_to_upload, "rb") as f:
+    s3_client.upload_fileobj(f, BUCKET_NAME, "example.txt")
+
+# before we can delete the bucket, we need to delete all the objects:
+bucket = s3_resource.Bucket(BUCKET_NAME)
+
+objects = []
+for object in bucket.object_versions.all():
+    objects.append({"Key": object.object_key, "VersionId": object.id})
+print(objects)
+bucket.delete_objects(Delete={"Objects": objects})
+"""
+Objects takes the following format:
+'Objects': [
+    {
+        'Key': 'string',
+        'VersionId': 'string'
+    },
+],
+"""
+# now that the bucket is empyty, we can delete the bucket:
+delete_response = s3_resource.Bucket(BUCKET_NAME).delete()
+
+print("Fin.")
